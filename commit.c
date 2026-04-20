d64950312eefb4d93ca057f14e268e76b36601ab
useramc <chinmayiamc@gmail.com>
2026-04-20 15:15:30 +0000

    Phase 4: started commit_create TODO implementation

--- commit.c
+++ commit.c
@@ -194,8 +194,34 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    Commit commit;
+    void *data = NULL;
+    size_t len = 0;
+    ObjectID parent_id;
+
+    if (!message || !commit_id_out) return -1;
+    memset(&commit, 0, sizeof(commit));
+
+    if (tree_from_index(&commit.tree) != 0) return -1;
+
+    if (head_read(&parent_id) == 0) {
+        commit.parent = parent_id;
+        commit.has_parent = 1;
+    } else {
+        commit.has_parent = 0;
+    }
+
+    snprintf(commit.author, sizeof(commit.author), "%s", pes_author());
+    commit.timestamp = (uint64_t)time(NULL);
+    snprintf(commit.message, sizeof(commit.message), "%s", message);
+
+    if (commit_serialize(&commit, &data, &len) != 0) return -1;
+    if (object_write(OBJ_COMMIT, data, len, commit_id_out) != 0) {
+        free(data);
+        return -1;
+    }
+    free(data);
+
+    if (head_update(commit_id_out) != 0) return -1;
+    return 0;
 }
