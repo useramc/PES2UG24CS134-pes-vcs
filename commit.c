156af078af7269fb70dff689f3ee677ada259ba8
useramc <chinmayiamc@gmail.com>
2026-04-20 15:18:15 +0000

    Phase 4: Updated Logic

--- commit.c
+++ commit.c
@@ -221,5 +221,7 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
         return -1;
     }
     free(data);
+     if (head_update(commit_id_out) != 0) return -1;
+    return 0;
 
 }
