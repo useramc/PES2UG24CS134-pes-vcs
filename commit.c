fa8d8257a70b5c28dbb45026ea3ee127fc12334e
useramc <chinmayiamc@gmail.com>
2026-04-20 15:17:32 +0000

    Phase 4: completed commit struct and serialization

--- commit.c
+++ commit.c
@@ -211,5 +211,15 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
         commit.has_parent = 0;
     }
 
-  
+   snprintf(commit.author, sizeof(commit.author), "%s", pes_author());
+    commit.timestamp = (uint64_t)time(NULL);
+    snprintf(commit.message, sizeof(commit.message), "%s", message);
+
+    if (commit_serialize(&commit, &data, &len) != 0) return -1;
+    if (object_write(OBJ_COMMIT, data, len, commit_id_out) != 0) {
+        free(data);
+        return -1;
+    }
+    free(data);
+
 }
